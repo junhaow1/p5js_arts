d5ffe92afb7636dcd3d51fd3b4b4dda46c0e78f4
kev <u7274552@anu.edu.au>
2022-10-04 00:29:22 +1100

    1

--- sketch.js
+++ sketch.js
@@ -1,48 +1,78 @@
 let Sound,amp,fft
-//预读器
+let xxx = 300;
+let yyy = 300;
+const easing = 0.1;
+const start1 = 0;
+const stop1 = 1;
+const start2 = 0;
+const stop2 = 150;
+const half = 2;
+const triple = 3;
+const zero = 0;
+// const
+
+
+
+
 function preload(){
     Sound = loadSound('timbre.flac');
 }
-//初始化
 function setup(){
     createCanvas(windowWidth,windowHeight);
     amp = new p5.Amplitude();
     noStroke();
     fft = new p5.FFT();
     frameRate(20);
+    osc = new p5.Oscillator();
+    osc.setType("sine");
+    osc.start();
+
 }
 function draw(){
     background(87);
-    //振幅转换为图形
-    let level = amp.getLevel();
-    let r = map(level,0,1,0,150);
-    fill(random(255),80,80);
-    ellipse(width/2,height/2,r,r);
-    //振幅转换为波形
+
+    osc.amp(mouseY/height);
+    osc.freq(mouseY*0.5);
+
+    fill(255,255,255);
+
+    ellipse(width/half,height/half,triple*map(amp.getLevel(),start1,stop1,start2,stop2),triple*map(amp.getLevel(),start1,stop1,start2,stop2));
     let waveform = fft.waveform();
     noFill();
-    stroke(255,80,80,60);
-    strokeWeight(2);
+    stroke(zero,zero,zero,60);
+
+    strokeWeight(4);
     let x;
     let y;
     let x1;
     let y1;
-    for (let i = 0; i < waveform.length; i++) {
-        let angle = map(i, 0, 1024, 0, 360);
-        x = width / 2 + cos(angle) * 180;
-        y = height / 2 + sin(angle) * 180;
+    for (let i = zero; i < waveform.length; i++) {
+        let angle = map(i, zero, 1024, zero, 360);
+        x = width / half + cos(angle) * 180;
+        y = height / half + sin(angle) * 180;
         let r = map(waveform[i], -0.8, 0.8, 50, 220);
-        x1 = width / 2 + cos(angle) * r;
-        y1 = height / 2 + sin(angle) * r;
+        x1 = width / half + cos(angle) * r*half;
+        y1 = height / half + sin(angle) * r*half;
         line(x, y, x1, y1);
     }
+
+    xxx=xxx+((mouseX-xxx)*easing);
+
+    yyy=yyy+((mouseY-yyy)*easing);
+        ellipse(xxx,yyy,75,75);
+        ellipse(mouseX,mouseY,50,50);
+
+
 }
-//点击播放暂停
 function mousePressed(){
-    if(Sound.isPlaying()){
-        Sound.pause();
-    }else{
+    // Sound.play();
+    if (Sound.isPlaying()) {
+        Sound.stop();
+        osc.stop(zero);
+        background(255, zero, zero);
+    } else {
         Sound.play();
-        background(255,255,0);
+        osc.start(zero,mouseY*0.5)
+        background(zero, 255, zero);
     }
 }
